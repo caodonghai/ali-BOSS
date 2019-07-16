77bf3aa6c56123e411020ce44a3048928f968fd4
wangbo <707607105@qq.com>
2019-07-16 16:29:35 +0800

    add:添加部分类型声明

--- src/app/interface.ts
+++ src/app/interface.ts
@@ -5,7 +5,11 @@ export interface Response {
   resMsg: string;
 }
 
-// ztree配置
+/**
+ * zTree类型声明
+ * 参考 http://www.treejs.cn/v3/api.php
+ * 暂时只写了一部分声明，未添加注释
+ */
 export interface ZTreeSetting {
   async?: ZTreeAsync;
   callback?: ZTreeCallback;
@@ -22,13 +26,44 @@ interface ZTreeAsync {
   dataType?: string;
   dataFilter?: any;
   otherParam?: string[];
-  type?: 'post'|'get';
+  type?: 'post' | 'get';
   header?: any;
   xhrFields?: any;
   url: string;
 }
 
+/**
+ * zTree节点
+ */
+interface ZTreeNode {
+  checked?: boolean;
+  children?: ZTreeNode[];
+  chkDisabled?: boolean;
+  click?: string;
+  isParent?: boolean;
+  isHidden?: boolean;
+  name?: string;
+  nocheck?: boolean;
+  open?: boolean;
+  target?: string;
+  url?: string;
+
+  [key: string]: any;
+}
+
+/**
+ * 回调函数
+ */
 interface ZTreeCallback {
+  beforeAsync?: (treeId: string, treeNode: ZTreeNode) => boolean;
+  beforeCheck?: (treeId: string, treeNode: ZTreeNode) => boolean;
+  beforeClick?: (treeId: string, treeNode: ZTreeNode, flag: boolean) => boolean;
+  beforeCollapse?: (treeId: string, treeNode: ZTreeNode) => boolean;
+  beforeDblClick?: (treeId: string, treeNode: ZTreeNode) => boolean;
+  beforeDrag?: (treeId: string, treeNode: ZTreeNode) => boolean;
+  beforeDragOpen?: (treeId: string, treeNode: ZTreeNode) => boolean;
+  beforeDrop?: (treeId: string, treeNode: ZTreeNode, targetNode: ZTreeNode, moveType: 'inner' | 'prev' | 'next', isCopy: boolean) => boolean;
+  onClick?: (event: MouseEvent, treeId: string, treeNode: ZTreeNode, clickFlag: 0 | 1 | 2) => boolean;
   beforeRename?: any;
 
   [key: string]: any;
